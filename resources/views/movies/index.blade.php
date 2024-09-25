ed0ce9c12d244eed54e84751fc702eba7530ef13
pierobramanti <piero.bramanti94@gmail.com>
2024-09-25 18:06:29 +0200

    [UPD] used foreach to cicle $movie in cards

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,17 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-@php
-var_dump($movie);
-@endphp
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>i film di Boolean</h1>
+
+<div class="container">
+    <div class="row">
+        @foreach ($movie as $movie)
+            <div class="col-md-3 col-sm-6 mb-4 mt-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $movie->title }}</h4>
+                        <h5 class="card-title">Originale: {{ $movie->original_title }}</h5>
+                        <p class="card-text">Voto: {{ $movie->vote }}/10</p>
+                        <p class="card-text">Data di uscita: {{ $movie->date }}</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-12">
-                
-            </div>
-        </div>
+        @endforeach
     </div>
+</div>
+
+
+ 
 @endsection
